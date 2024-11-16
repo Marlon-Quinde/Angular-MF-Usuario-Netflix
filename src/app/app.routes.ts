edd39a61b94b9124e005635a41657219f136af00
mquinde <mquinde@viamatica.me>
2024-11-15 21:35:08 -0500

    feat(Structure): :sparkles: Modificaciones a la estrcutura del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './module/home/home.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    pathMatch: 'full',
-  },
+
   {
     path: 'usuario',
     loadChildren: () => import('./module/usuario/usuario.module').then((m) => m.UsuarioModule)
